c35bbbcf8974c8eaf18c4e210e316fecae1dbfc0
ukashakabandayawe <83324428+ukashakabandayawe@users.noreply.github.com>
2026-01-03 17:24:03 +0300

    Correct dictionary file loading to use "Luganda" instead of "New" for accurate word checks

--- check_word.py
+++ check_word.py
@@ -1,8 +1,11 @@
 from spylls.hunspell import Dictionary
 
-# Loads New.aff + New.dic (same basename: "New")
-d = Dictionary.from_files("New")
+# Loads Luganda.aff + Luganda.dic (same basename: "Lugnada")
+d = Dictionary.from_files("Luganda")
 
 words = ["luma", "baluma", "ziluma", "baziluma", "zibaluma"]
 for w in words:
     print(f"{w}: {'OK' if d.lookup(w) else 'NO'}")
+
+# for suggestion in d.suggest('spylls'):
+#         print(sugestion)
